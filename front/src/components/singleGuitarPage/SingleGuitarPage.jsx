0e1d7b3b3d915e745597d4209f4aa901ad6f1acd
split73 <54321lexa12345@mail.ru>
2024-01-16 15:25:05 +0400

    add overwiew specifiacation

--- front/src/components/singleGuitarPage/SingleGuitarPage.jsx
+++ front/src/components/singleGuitarPage/SingleGuitarPage.jsx
@@ -1,17 +1,22 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import "./singleGuitarPage.css"
+import { Button } from 'react-bootstrap';
 
 const SingleGuitarPage = () => {
-  const [fetchData, setFetchData] = useState();
+  const [fetchData, setFetchData] = useState({});
   const [imageToDisplay, setImageToDisplay] = useState("placeHolder");
   const [imageStyle, setImageStyle] = useState({backgroundImage: `url(${imageToDisplay})`,backgroundPosition: '0% 0%'})
-
+  
   const handleZoomOnHover = (e) => {
     const {left, top, width, height} = e.target.getBoundingClientRect();
     const x = (e.pageX - left) / width * 100;
     const y = (e.pageY - top) / height * 100;
     setImageStyle({ ...imageStyle, backgroundPosition: `${x}% ${y}%`})
+  }
+
+  const handleLogFetch = () => {
+    console.log(fetchData[0])
   }
 
   useEffect(() => {
@@ -34,7 +39,7 @@ const SingleGuitarPage = () => {
      )
 
     setFetchData(res.data[0])
-        console.log(res)
+
     if(res.data[0].fullresolutionimageurl){
       setImageToDisplay(res.data[0].fullresolutionimageurl)
       setImageStyle({backgroundImage: `url(${res.data[0].fullresolutionimageurl})`,backgroundPosition: '0% 0%'})
@@ -50,12 +55,23 @@ const SingleGuitarPage = () => {
   }, [])
 
   return (
-    <div>
+    <div className='wrapper'>
       <figure className='main-image-figure' onMouseMove={handleZoomOnHover} style={imageStyle}>
         <img className='main-image' src={imageToDisplay} />
       </figure>
       <div className='main-info'>
-        <h1 className='title'>{}</h1>
+        <h1 className='title'>{fetchData[0]?.name}</h1>
+        <h2 className='price'>{fetchData[0]?.price}</h2>
+      </div>
+      <Button onClick={handleLogFetch} style={{width: 100}}>LOG</Button>
+      <div className='wrapper-overview-specs'>
+        <h1 className='select-overview-specs'>
+          <Button>overview</Button>
+          <Button>specification</Button>
+        </h1>
+        <p className='overview-specs'>
+          {fetchData[0]?.overview}
+        </p>
       </div>
     </div>
   )
