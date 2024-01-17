b2b6521f3b46b7f0854232afe31ebbb5c72691c5
split73 <54321lexa12345@mail.ru>
2024-01-17 13:45:05 +0400

    overview specifiacation toggle

--- front/src/components/singleGuitarPage/SingleGuitarPage.jsx
+++ front/src/components/singleGuitarPage/SingleGuitarPage.jsx
@@ -7,9 +7,12 @@ const SingleGuitarPage = () => {
   const [fetchData, setFetchData] = useState({});
   const [imageToDisplay, setImageToDisplay] = useState("placeHolder");
   const [imageStyle, setImageStyle] = useState({backgroundImage: `url(${imageToDisplay})`,backgroundPosition: '0% 0%'});
-  const [overview, setOverview] = useState("PLACEHOLDER");
-  const [specification, setSpecification] = useState("PLACEHOLDER");
-  
+  const [overview, setOverview] = useState(["PLACEHOLDER"]);
+  const [specification, setSpecification] = useState(["PLACEHOLDER"]);
+  const [showToggle, setShowToggle] = useState(true)
+  const [keyFeatures, setKeyFeatures] = useState(["placeholder"])
+  const [specs, setSpecs] = useState([]);
+
   const handleZoomOnHover = (e) => {
     const {left, top, width, height} = e.target.getBoundingClientRect();
     const x = (e.pageX - left) / width * 100;
@@ -19,6 +22,14 @@ const SingleGuitarPage = () => {
 
   const handleLogFetch = () => {
     console.log(fetchData[0])
+  }
+
+  const handleShowOverview = () => {
+    setShowToggle(true)
+  }
+
+  const handleShowSpecification = () => {
+    setShowToggle(false)
   }
 
   useEffect(() => {
@@ -40,21 +51,37 @@ const SingleGuitarPage = () => {
          `http://localhost:8080/api/get-single-product/${page}`
      )
 
-
     let tmpSpecefication = [];
+    let tmpOverview = [];
+    let tmpKeyFeatures = [];
+    let tmpSpecs = [];
 
-      for (let i = 0; i < res.data[0].specification.length; i++){
-        tmpSpecefication.push(JSON.parse(res.data[0].specification[i]))
-      }
-      console.log(tmpSpecefication)
+    for (let i = 0; i < res.data[0].specification.length; i++){
+      tmpSpecefication.push(JSON.parse(res.data[0].specification[i]))
+    }
+
+    for (let i = 0; i < res.data[0].overview.length; i++){
+      tmpOverview.push(JSON.parse(res.data[0].overview[i]))
+    }
+
+    for (let i = 0; i < res.data[0].keyfeatures.length; i++){
+      tmpKeyFeatures.push(JSON.parse(res.data[0].keyfeatures[i]))
+    }
+
+    for (let i = 0; i < res.data[0].specs.length; i++){
+      tmpSpecs.push(JSON.parse(res.data[0].specs[i]))
+    }
+
+    console.log(tmpSpecs[0].specsBody)
+
     setSpecification(tmpSpecefication)
-    
+    setOverview(tmpOverview)
+    setKeyFeatures(tmpKeyFeatures)
+    setSpecs(tmpSpecs)
 
     setFetchData(res.data[0])
-
-    setOverview(JSON.parse(res.data[0].overview).overviewBody)
-
     
+
 
     if(res.data[0].fullresolutionimageurl){
       setImageToDisplay(res.data[0].fullresolutionimageurl)
@@ -83,21 +110,38 @@ const SingleGuitarPage = () => {
       <div className='wrapper-overview-specs'>
         <h1 className='select-overview-specs'>
         <Button onClick={handleLogFetch} style={{width: 100}}>LOG</Button>
-          <Button>overview</Button>
-          <Button>specification</Button>
+          <Button onClick={handleShowOverview}>overview</Button>
+          <Button onClick={handleShowSpecification}>specification</Button>
         </h1>
         <p className='overview-specs'>
-          {overview}
+          {showToggle && overview.map((e, index) => (
+                <em key={index + "over"} className='overview'>{e.overviewBody}</em> 
+            ))}
           <ul>
-            {specification.map((e) => (
-              <li>
-                <h1>{e.keySpecification}</h1> 
-                <h1>{e.valueSpecification}</h1>
+            {showToggle && keyFeatures.map((e, index) => (
+              <li key={index + "keyFeat"}>
+                <em className='overview'>{e.keyFeaturesBody}</em> 
               </li>
+                
             ))}
           </ul>
           
+          <ul>
+            {showToggle && specs.map((e, index) => (
+              <li key={index + "spec"}>
+                <em className='overview'>{e.specsBody}</em> 
+              </li>
+            ))} 
+          </ul>
+           
         </p>
+        <ul>
+            {!showToggle && specification.map((e, index) => (
+              <li key={index + "spec"}>
+                <p className='specification'>{e.keySpecification}{e.valueSpecification}</p> 
+              </li>
+            ))}
+          </ul>
       </div>
     </div>
   )
